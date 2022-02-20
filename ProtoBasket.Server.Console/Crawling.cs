c33e8d43bd2657580addd73bcb96770dc7f8b619
Jaywapp <jaywapp16@gmail.com>
2022-02-20 22:06:33 +0900

    Add crawling functions, match models

---
+++ ProtoBasket.Server.Console/Crawling.cs
@@ -0,0 +1,21 @@
+﻿using ProtoBasket.Server.Crawler.Model;
+using ProtoBasket.Server.Crawler.Service;
+
+namespace ProtoBasket.Server.Console
+{
+    public static class Crawling
+    {
+        public static void PrintProto(int year, int no)
+        {
+            var url = new ProtoURL(year, no);
+            var proto = url.Create();
+
+            System.Console.WriteLine($"프로토 {url.ProtoNo}회차");
+
+            foreach (var match in proto.Matches)
+                System.Console.WriteLine(match.ToString());
+
+            System.Console.ReadLine();
+        }
+    }
+}
